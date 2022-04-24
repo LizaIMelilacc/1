325f81f1182d6c9140ab04b21bd39d414fc83737
BigCubeCat <bigcubecat@yandex.ru>
2022-04-25 01:09:00 +0700

    add pretty_recipe() not finished

--- api.py
+++ api.py
@@ -4,6 +4,22 @@ from dotenv import load_dotenv
 
 load_dotenv()
 API_ADDRESS = os.getenv("API_ADDRESS")
+SITE = os.getenv("SITE")
+
+
+def pretty_recipe(recipe_json):
+    """
+    :param recipe_json: JSON response from search API
+    :return: user-readable text
+    """
+    new_line = '\n'
+    # TODO: not finished
+    return \
+        f"""
+        {recipe_json['Title']}
+        {recipe_json['Description']}
+        {recipe_json['Steps'].replace(';', new_line * 2)}
+        """
 
 
 def get_by_ingredients(good=[], bad=[]):
@@ -11,13 +27,16 @@ def get_by_ingredients(good=[], bad=[]):
     post request to search-server
     :param good: preferred ingredients
     :param bad: stop-list ingredients
-    :return: message
+    :return: None, if not found else
     """
     response = requests.get(API_ADDRESS + "/find", json={
         "good": good,
         "bad": bad
     })
-    print(response.text)
+    if response.status_code != 200:
+        return None
+
+    return pretty_recipe(response.json()['response'])
 
 
 def get_by_title(title):
@@ -30,4 +49,4 @@ def get_by_title(title):
 
 
 if __name__ == "__main__":
-    get_by_ingredients(["молоко"], [])
+    print(get_by_ingredients(["молоко", "яйца"], ["рис"]))
